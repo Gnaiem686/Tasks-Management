d3a7199bc784fe84eb1c6fbb0997dc74fe5ac298
Mohammad Ganaiem <gnaiem686@gmail.com>
2026-08-11 11:00:19 +0300

    fix: reuse account github oidc provider

--- infra/terraform/shared/main.tf
+++ infra/terraform/shared/main.tf
@@ -47,10 +47,8 @@ resource "aws_ecr_lifecycle_policy" "application" {
   })
 }
 
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
 }
 
 data "aws_iam_policy_document" "github_environment_trust" {
@@ -61,7 +59,7 @@ data "aws_iam_policy_document" "github_environment_trust" {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
     }
     condition {
       test     = "StringEquals"
